4577e8a027e7fcc32f0884bdae303b7e135c7a73
ByteJoseph <ByteJoseph@users.noreply.github.com>
2026-06-01 04:28:34 +0000

    deploy: 9f33b77c50e169bb22a8537a7184b14a81a723c8

--- search/defines_11.js
+++ search/defines_11.js
@@ -1,15 +1,16 @@
 var searchData=
 [
-  ['qemu_5fvirt_5frtcirq_0',['QEMU_VIRT_RTCIRQ',['../rtcmmio_8c.html#a8817a211fca1e9bee43290a773db314a',1,'rtcmmio.c']]],
-  ['qf_5fdate_5fstorage_5fnormal_1',['QF_DATE_STORAGE_NORMAL',['../UefiInternalFormRepresentation_8h.html#ae6cdc50a19d621e7a0fb4e83e7173dfb',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5fdate_5fstorage_5ftime_2',['QF_DATE_STORAGE_TIME',['../UefiInternalFormRepresentation_8h.html#a8749924c956171ac8573aecdfbf70387',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5fdate_5fstorage_5fwakeup_3',['QF_DATE_STORAGE_WAKEUP',['../UefiInternalFormRepresentation_8h.html#aa3254f13ae98bf9af5451d484afde344',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5ftime_5fhour_5fsuppress_4',['QF_TIME_HOUR_SUPPRESS',['../UefiInternalFormRepresentation_8h.html#aaa51a2c91b9033587e35751f9391816a',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5ftime_5fminute_5fsuppress_5',['QF_TIME_MINUTE_SUPPRESS',['../UefiInternalFormRepresentation_8h.html#a50e171281fe32d3f2e01eba500ba6010',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5ftime_5fsecond_5fsuppress_6',['QF_TIME_SECOND_SUPPRESS',['../UefiInternalFormRepresentation_8h.html#a31c9b57e459c5e592a641e817912e172',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5ftime_5fstorage_7',['QF_TIME_STORAGE',['../UefiInternalFormRepresentation_8h.html#afbed859de61bb464cd0361531415b376',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5ftime_5fstorage_5fnormal_8',['QF_TIME_STORAGE_NORMAL',['../UefiInternalFormRepresentation_8h.html#a62f724fe98525ee94421aeb87f2aefc4',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5ftime_5fstorage_5ftime_9',['QF_TIME_STORAGE_TIME',['../UefiInternalFormRepresentation_8h.html#ac29b5768fbffad89796d0ec7fb2b7dc6',1,'UefiInternalFormRepresentation.h']]],
-  ['qf_5ftime_5fstorage_5fwakeup_10',['QF_TIME_STORAGE_WAKEUP',['../UefiInternalFormRepresentation_8h.html#aa391b102dd76c6a8fe9254459200d209',1,'UefiInternalFormRepresentation.h']]],
-  ['queue_5fwork_11',['queue_work',['../workqueue_8h.html#aa5205ac7aa2ee85607308b06e2561cc2',1,'workqueue.h']]]
+  ['qemu_5fvirt_5fecam_5fbase_0',['QEMU_VIRT_ECAM_BASE',['../pcie_8c.html#ae0aefadaeef481211794cabd9d053756',1,'pcie.c']]],
+  ['qemu_5fvirt_5frtcirq_1',['QEMU_VIRT_RTCIRQ',['../rtcmmio_8c.html#a8817a211fca1e9bee43290a773db314a',1,'rtcmmio.c']]],
+  ['qf_5fdate_5fstorage_5fnormal_2',['QF_DATE_STORAGE_NORMAL',['../UefiInternalFormRepresentation_8h.html#ae6cdc50a19d621e7a0fb4e83e7173dfb',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5fdate_5fstorage_5ftime_3',['QF_DATE_STORAGE_TIME',['../UefiInternalFormRepresentation_8h.html#a8749924c956171ac8573aecdfbf70387',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5fdate_5fstorage_5fwakeup_4',['QF_DATE_STORAGE_WAKEUP',['../UefiInternalFormRepresentation_8h.html#aa3254f13ae98bf9af5451d484afde344',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5ftime_5fhour_5fsuppress_5',['QF_TIME_HOUR_SUPPRESS',['../UefiInternalFormRepresentation_8h.html#aaa51a2c91b9033587e35751f9391816a',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5ftime_5fminute_5fsuppress_6',['QF_TIME_MINUTE_SUPPRESS',['../UefiInternalFormRepresentation_8h.html#a50e171281fe32d3f2e01eba500ba6010',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5ftime_5fsecond_5fsuppress_7',['QF_TIME_SECOND_SUPPRESS',['../UefiInternalFormRepresentation_8h.html#a31c9b57e459c5e592a641e817912e172',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5ftime_5fstorage_8',['QF_TIME_STORAGE',['../UefiInternalFormRepresentation_8h.html#afbed859de61bb464cd0361531415b376',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5ftime_5fstorage_5fnormal_9',['QF_TIME_STORAGE_NORMAL',['../UefiInternalFormRepresentation_8h.html#a62f724fe98525ee94421aeb87f2aefc4',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5ftime_5fstorage_5ftime_10',['QF_TIME_STORAGE_TIME',['../UefiInternalFormRepresentation_8h.html#ac29b5768fbffad89796d0ec7fb2b7dc6',1,'UefiInternalFormRepresentation.h']]],
+  ['qf_5ftime_5fstorage_5fwakeup_11',['QF_TIME_STORAGE_WAKEUP',['../UefiInternalFormRepresentation_8h.html#aa391b102dd76c6a8fe9254459200d209',1,'UefiInternalFormRepresentation.h']]],
+  ['queue_5fwork_12',['queue_work',['../workqueue_8h.html#aa5205ac7aa2ee85607308b06e2561cc2',1,'workqueue.h']]]
 ];
