fd7d1f6e48807fec1639154690150ded3c0a0581
ByteJoseph <ByteJoseph@users.noreply.github.com>
2026-07-21 03:02:13 +0000

    deploy: 9f33b77c50e169bb22a8537a7184b14a81a723c8

--- search/typedefs_17.js
+++ search/typedefs_17.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['wchar_5ft_0',['wchar_t',['../stddef_8h.html#a2e9a8173645cde5c5e7ae1eaa41d27ce',1,'stddef.h']]],
+  ['wchar_5ft_0',['wchar_t',['../Libs_2XEClib_2includes_2stddef_8h.html#a2e9a8173645cde5c5e7ae1eaa41d27ce',1,'wchar_t:&#160;stddef.h'],['../BaseHdr_2stddef_8h.html#a2e9a8173645cde5c5e7ae1eaa41d27ce',1,'wchar_t:&#160;stddef.h']]],
   ['window_1',['window',['../Process_2Deodhai_2window_8h.html#a8992679d9f1b8c5d1cd6a3f8979e0573',1,'Window:&#160;window.h'],['../Process_2DeodhaiXR_2window_8h.html#a8992679d9f1b8c5d1cd6a3f8979e0573',1,'Window:&#160;window.h']]],
   ['winsharedinfo_2',['winsharedinfo',['../Process_2Deodhai_2window_8h.html#a53bc978102cea75fdf6b7b9adbf36d2c',1,'WinSharedInfo:&#160;window.h'],['../Process_2DeodhaiXR_2window_8h.html#a53bc978102cea75fdf6b7b9adbf36d2c',1,'WinSharedInfo:&#160;window.h']]],
   ['winsize_3',['winsize',['../tty_8h.html#a0dd034de36fe76fed7b2f793fe783588',1,'WinSize:&#160;tty.h'],['../__ketty_8h.html#a0dd034de36fe76fed7b2f793fe783588',1,'WinSize:&#160;_ketty.h']]],
-  ['wint_5ft_4',['wint_t',['../stddef_8h.html#ae66a5a4c3f483e5c7705ba6ca94a4455',1,'stddef.h']]],
+  ['wint_5ft_4',['wint_t',['../Libs_2XEClib_2includes_2stddef_8h.html#aae8fec8a77257a9718887040067a3f09',1,'wint_t:&#160;stddef.h'],['../BaseHdr_2stddef_8h.html#aae8fec8a77257a9718887040067a3f09',1,'wint_t:&#160;stddef.h']]],
   ['woff_5fheader_5',['WOFF_Header',['../tttypes_8h.html#aed034e10679ba37540650cbc310fb932',1,'tttypes.h']]],
   ['woff_5fheaderrec_6',['WOFF_HeaderRec',['../tttypes_8h.html#ade56c46ffbba1c7e8e5c0a242056c28c',1,'tttypes.h']]],
   ['woff_5ftable_7',['WOFF_Table',['../tttypes_8h.html#ab98ded8549fb113503e693c88e05d4fb',1,'tttypes.h']]],
