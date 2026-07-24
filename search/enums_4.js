960d7d29de2641605b972c81cf64e32f733719b6
ByteJoseph <ByteJoseph@users.noreply.github.com>
2026-07-24 03:00:48 +0000

    deploy: 9f33b77c50e169bb22a8537a7184b14a81a723c8

--- search/enums_4.js
+++ search/enums_4.js
@@ -4,5 +4,6 @@ var searchData=
   ['dhcp6_5fduid_5ftype_1',['DHCP6_DUID_TYPE',['../Dhcp_8h.html#a86f7ee110827199809e08cfafa25aec2',1,'Dhcp.h']]],
   ['dhcp6_5fmsg_5ftype_2',['DHCP6_MSG_TYPE',['../Dhcp_8h.html#a735b1e90b9a9be8c14fd3390891c3ec1',1,'Dhcp.h']]],
   ['dhcp6_5fopt_5fcode_3',['DHCP6_OPT_CODE',['../Dhcp_8h.html#a36c6dc3ed1e40cfff12317508fbc38fa',1,'Dhcp.h']]],
-  ['dhcp6_5fsts_5fcode_4',['DHCP6_STS_CODE',['../Dhcp_8h.html#a26b00fbce2cd07042c94751b1356a29b',1,'Dhcp.h']]]
+  ['dhcp6_5fsts_5fcode_4',['DHCP6_STS_CODE',['../Dhcp_8h.html#a26b00fbce2cd07042c94751b1356a29b',1,'Dhcp.h']]],
+  ['driver_5ftype_5',['driver_type',['../core_8h.html#a6bb533f22fc3ffa26a6602df217d0d0f',1,'core.h']]]
 ];
