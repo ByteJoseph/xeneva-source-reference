0f725ce2495aaf62f7ebf0f25432c884a38e147d
ByteJoseph <ByteJoseph@users.noreply.github.com>
2026-07-31 03:25:44 +0000

    deploy: 9f33b77c50e169bb22a8537a7184b14a81a723c8

--- search/enums_2.js
+++ search/enums_2.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['base_5fboard_5ftype_0',['BASE_BOARD_TYPE',['../IndustryStandard_2SmBios_8h.html#a3e8ce1de548fa87a4e4c1e8aeca378a6',1,'SmBios.h']]],
   ['bdf_5fpropertytype_5f_1',['BDF_PropertyType_',['../ftbdf_8h.html#a1c5af1ece15330758f3698fd6ac8a277',1,'ftbdf.h']]],
-  ['bloglevel_2',['BLogLevel',['../klog_8h.html#a3441fcd3cbcc6ae3d2c6fc523bd5b3ce',1,'klog.h']]],
-  ['bmc_5finterface_5ftype_3',['BMC_INTERFACE_TYPE',['../IndustryStandard_2SmBios_8h.html#a36b0cee526945dd75878a4af00cdd0d2',1,'SmBios.h']]],
-  ['builtin_5fpointing_5fdevice_5finterface_4',['BUILTIN_POINTING_DEVICE_INTERFACE',['../IndustryStandard_2SmBios_8h.html#a8efdcbb2207d18f87e3011cd0971c73e',1,'SmBios.h']]],
-  ['builtin_5fpointing_5fdevice_5ftype_5',['BUILTIN_POINTING_DEVICE_TYPE',['../IndustryStandard_2SmBios_8h.html#a92d09270c6d6cb8af6c03011f7737fcb',1,'SmBios.h']]]
+  ['blk_5fmap_5fid_2',['blk_map_id',['../imx8mp__blkctrl_8h.html#aacae50c1a80fd7936457d804feda68e0',1,'imx8mp_blkctrl.h']]],
+  ['bloglevel_3',['BLogLevel',['../klog_8h.html#a3441fcd3cbcc6ae3d2c6fc523bd5b3ce',1,'klog.h']]],
+  ['bmc_5finterface_5ftype_4',['BMC_INTERFACE_TYPE',['../IndustryStandard_2SmBios_8h.html#a36b0cee526945dd75878a4af00cdd0d2',1,'SmBios.h']]],
+  ['builtin_5fpointing_5fdevice_5finterface_5',['BUILTIN_POINTING_DEVICE_INTERFACE',['../IndustryStandard_2SmBios_8h.html#a8efdcbb2207d18f87e3011cd0971c73e',1,'SmBios.h']]],
+  ['builtin_5fpointing_5fdevice_5ftype_6',['BUILTIN_POINTING_DEVICE_TYPE',['../IndustryStandard_2SmBios_8h.html#a92d09270c6d6cb8af6c03011f7737fcb',1,'SmBios.h']]]
 ];
